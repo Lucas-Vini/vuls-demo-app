81c152844e1007977749c7991fa146354e9ba927
Lucas-Vini <lvinicius31@gmail.com>
2025-08-10 20:39:24 -0300

    receiving data from user

--- app/interfaces/http/controllers/user_controller.py
+++ app/interfaces/http/controllers/user_controller.py
@@ -1,7 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 user = Blueprint("user", __name__)
 
 @user.route("/signup")
 def main():
+	data = request.get_json() or {}
+	username = data.get("username")
+	password = data.get("password")
+
 	return "This endpoint will be used to register users"
