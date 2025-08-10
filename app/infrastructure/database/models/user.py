8cf3c1564fab3ffb14ce9d4cbb06eedfc2dee26a
Lucas-Vini <lvinicius31@gmail.com>
2025-08-10 19:26:09 -0300

    user model

---
+++ app/infrastructure/database/models/user.py
@@ -0,0 +1,19 @@
+from app.infrastructure.db import db
+from app.domain.entities.user import User
+
+class UserModel(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+
+    def to_entity(self) -> User:
+        return User(id=self.id, username=self.username, password_hash=self.password_hash)
+
+    @classmethod
+    def from_entity(cls, user: User) -> UserModel:
+        kwargs = {"username": user.username, "password_hash": user.password_hash}
+        if user.id is not None:
+            kwargs["id"] = user.id
+        return cls(**kwargs)
