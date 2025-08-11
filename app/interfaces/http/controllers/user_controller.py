6ea045ca671b6b5a959f6a8dd5bbc6db53a3a2f2
Lucas-Vini <lvinicius31@gmail.com>
2025-08-11 15:26:58 -0300

    including sqlalchemy user repository in user controller

--- app/interfaces/http/controllers/user_controller.py
+++ app/interfaces/http/controllers/user_controller.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request
+from app.infrastructure.database.repositories.user_repository_implementation import SqlAlchemyUserRepository
 
 user = Blueprint("user", __name__)
 
@@ -7,5 +8,7 @@ def main():
 	data = request.get_json() or {}
 	username = data.get("username")
 	password = data.get("password")
+
+	sql_alchemy_user_repository = SqlAlchemyUserRepository()
 
 	return "This endpoint will be used to register users"
