054a13375318b91e832bdf54a6cb5a72120d5adc
Lucas-Vini <lvinicius31@gmail.com>
2025-08-12 16:17:40 -0300

    importing user entity to type hint usage

--- app/infrastructure/database/repositories/user_repository_implementation.py
+++ app/infrastructure/database/repositories/user_repository_implementation.py
@@ -1,6 +1,7 @@
 from app.infrastructure.database.db import db
 from app.infrastructure.database.models.user import UserModel
 from app.domain.repositories.user_repository import UserRepository
+from app.domain.entities.user import User
 
 class SqlAlchemyUserRepository(UserRepository):
     def add(self, user: User) -> User:
